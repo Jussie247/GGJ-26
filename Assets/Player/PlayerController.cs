e0b839c6fc93d77dc9eb4b359e1b7efda0399503
Denise Franz <denisealiciafranz@gmail.com>
2026-01-31 12:45:34 +0100

    basic charatcer controller

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 2f;
+    public InputActionReference move;
+    public InputActionReference interact;
+    private Vector2 moveDirection;
     void Start()
     {}
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        moveDirection = move.action.ReadValue<Vector2>(); 
+        transform.Translate(new Vector3(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime, 0));
+         
+        
 
-        transform.position = new Vector3(horizontal, vertical, 0) * moveSpeed * Time.deltaTime;
     }
 }
