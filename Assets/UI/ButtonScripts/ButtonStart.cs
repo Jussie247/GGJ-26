cb199137ac17e425881517e03c4ba0a1597a6079
Denise Franz <denisealiciafranz@gmail.com>
2026-01-31 16:30:07 +0100

    walking spritesheet & menu audio

--- Assets/UI/ButtonScripts/ButtonStart.cs
+++ Assets/UI/ButtonScripts/ButtonStart.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonStart : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Playground");
     }
 }
