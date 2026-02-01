7ba697d13a8dc02e08ca9078cc1eaadbf949640a
Denise Franz <denisealiciafranz@gmail.com>
2026-02-01 10:16:16 +0100

    Import Assets & Flip Sprite when walking

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,12 +7,23 @@ public class PlayerController : MonoBehaviour
     public InputActionReference move;
     public InputActionReference interact;
     private Vector2 moveDirection;
+    private bool isFacingRight = true;
     void Start()
     {}
     void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>(); 
         transform.Translate(new Vector3(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime, 0));
+        if (moveDirection.x > 0 && !isFacingRight)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            isFacingRight = true;
+        }
+        else if (moveDirection.x < 0 && isFacingRight)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            isFacingRight = false;
+        }
          
         
 
