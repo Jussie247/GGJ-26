5f36cfe1d34a8d0a19514c23da5374372f40f772
“Jussie247” <justus.schultzke@gmail.com>
2026-02-01 23:24:54 +0100

    Added credits and Home screen

--- Assets/UI/ButtonScripts/ButtonStart.cs
+++ Assets/UI/ButtonScripts/ButtonStart.cs
@@ -3,9 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class ButtonStart : MonoBehaviour
 {
+    [Header("Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject creditsPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Playground");
+    }
+
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 
     public void QuitGame()
